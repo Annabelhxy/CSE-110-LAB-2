231aa1675e2812cf345ab58fbac512f2191887a1
Annabelhxy <xih035@ucsd.edu>
2024-10-07 15:51:04 -0700

    part 4.2 done

---
+++ new-app/my-app/src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Label, Note } from "./types"; // Import the Label type from the appropriate module
+import { dummyNotesList } from "./constant"; // Import the dummyNotesList from the appropriate module
+import { ClickCounter } from './hooksExercise';
+import ToggleTheme from './hooksExercise';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app-container'>
+
+      <form className="note-form">
+        <div><input placeholder="Note Title"></input></div>
+
+        <div><textarea></textarea></div>
+
+        <div><button type="submit">Create Note</button></div>
+      </form>
+
+      <div className="notes-grid">
+        {dummyNotesList.map((note) => (
+          <div
+            key={note.id}
+            className="note-item">
+            <div className="notes-header">
+              <button>❤️</button>
+              <button>x</button>
+            </div>
+            <h2> {note.title} </h2>
+            <p> {note.content} </p>
+            <p> {note.label} </p>
+          </div>
+        ))}
+      </div>
+      <ToggleTheme />
+    </div>
+ 
+  );
+}
+ 
+export default App;
